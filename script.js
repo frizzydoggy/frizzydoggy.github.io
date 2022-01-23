2426b797fee04d0a4672b1983b5be3924af4cc13
miris <98238680+frizzydoggy@users.noreply.github.com>
2022-01-23 05:36:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function(){
     var ageElem = document.getElementById("age");
     ageElem.innerText = getAge("Dec 04 2007 18:00:00 GMT+0000");
 });
+document.getElementsByClassName("downbtn")[0].onclick = function(){
+ var box = document.getElementsByClassName("box")[0];
+ box.scrollTo({top: box.scrollHeight, behavior:'smooth'}); 
+}
 document.querySelectorAll('[data-link]').forEach(el=>{
 el.onclick = function() {
 window.open(el.dataset.link)
