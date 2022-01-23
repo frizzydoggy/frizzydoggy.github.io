8bbb81a62bdad2f8c0293c771de62fec782dc5ac
miris <98238680+frizzydoggy@users.noreply.github.com>
2022-01-23 04:15:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    var ageElem = document.getElementById("age");
+    ageElem.innerText = getAge("Dec 04 2007 18:00:00 GMT+0000");
 });
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
